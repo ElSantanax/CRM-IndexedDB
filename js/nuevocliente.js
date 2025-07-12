8cadf3ce90a593166e3c63163bf6f0b114a482f6
ElSantanax <josesantanaserrano130598@gmail.com>
2025-07-12 08:51:58 -0500

    Update: Alertas y validación

---
+++ js/nuevocliente.js
@@ -0,0 +1,59 @@
+(function () {
+    let DB;
+    const formulario = document.querySelector('#formulario');
+
+    document.addEventListener('DOMContentLoaded', () => {
+        // Conectar DB
+        conectarDB();
+
+        formulario.addEventListener('submit', validarCliente)
+    });
+
+    function conectarDB() {
+        // abri la conexión
+        const abrirConexion = window.indexedDB.open('crm', 1);
+
+        // 400
+        abrirConexion.onerror = function () {
+            console.log('Hubo un error al conectace con la base de datos ');
+        }
+
+        // 200
+        abrirConexion.onsuccess = function () {
+            DB = abrirConexion.result;
+        }
+    }
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+
+        if (nombre === '' || email === '' || telefono === '' || empresa === '') {
+            imprimirAlerta('Todos los campos son obligatorios', 'error');
+            return;
+        }
+    }
+
+    function imprimirAlerta(mensaje, tipo) {
+        // Crear Alerta
+        const alerta = document.querySelector('.alerta');
+
+        if (!alerta) {
+            const divMensaje = document.createElement('DIV');
+            divMensaje.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center', 'border', 'alerta');
+
+            if (tipo === 'error') {
+                divMensaje.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
+            } else {
+                divMensaje.classList.add('bg-green-100', 'border-green-400', 'text-green-700');
+            }
+
+            divMensaje.textContent = mensaje;
+            formulario.appendChild(divMensaje);
+        }
+    }
+})();
